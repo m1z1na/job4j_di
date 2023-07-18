8cb156bf7c3a38dcb7b73b27b9393da9cd817bf8
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-07-18 16:06:41 +0300

    1. Spring DI. Запуск [#2091  [#2091]]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,19 @@
+package ru.job4j.di;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        StartUI.ConsoleInput consoleInput = context.getBean(StartUI.ConsoleInput.class);
+        consoleInput.askStr("Do your know who is creator?");
+        ui.add("Petr Arsentev");
+        ui.print();
+    }
+}
